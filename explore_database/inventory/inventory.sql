e431cf19d8f236113f976ded05d4a6b805532544
ritik574-coder <ritik74820@gmail.com>
2026-05-16 17:36:16 +0530

    adding headling in invertory data column data

--- explore_database/inventory/inventory.sql
+++ explore_database/inventory/inventory.sql
@@ -24,12 +24,12 @@ SELECT TOP (1000) [snapshot_date]
 --=============================================================================================
 --============================== snapshot_date column cleaning ================================
 --=============================================================================================
--- snapshot_data overview 
+-- snapshot_date overview 
 SELECT 
     snapshot_date
 FROM bronze.inventory_snapshots ;
 
---- snapshot_data pattern analysis 
+-- snapshot_date pattern analysis 
 WITH date_pattern_analysis AS 
 (
     SELECT 
@@ -37,102 +37,110 @@ WITH date_pattern_analysis AS
             TRIM(LOWER(snapshot_date)), 
             '0123456789abcdefghijklmnopqrstuvwxyz',
             '9999999999aaaaaaaaaaaaaaaaaaaaaaaaaa'
-        ) as date_pattern 
+        ) AS date_pattern 
     FROM bronze.inventory_snapshots 
 )
 SELECT 
     date_pattern,
-    COUNT(*) as pattern_count,
-    CAST(ROUND(COUNT(*)*100.0/SUM(COUNT(*)) OVER() ,2) AS NVARCHAR) + '%' as percentage 
+    COUNT(*) AS pattern_count,
+    CAST(
+        ROUND(COUNT(*) * 100.0 / SUM(COUNT(*)) OVER(), 2) AS NVARCHAR
+    ) + '%' AS percentage 
 FROM date_pattern_analysis 
-    GROUP BY date_pattern
-    ORDER BY pattern_count DESC ;
+GROUP BY date_pattern
+ORDER BY pattern_count DESC ;
 
---
+-- MM/DD/YYYY format analysis 
 SELECT
     snapshot_date
 FROM bronze.inventory_snapshots
 WHERE snapshot_date LIKE '__/__/____' ;
 
+-- MM/DD/YYYY month validation check 
 SELECT
     snapshot_date
 FROM bronze.inventory_snapshots
-WHERE snapshot_date LIKE '__/__/____' AND SUBSTRING(snapshot_date, 4, 2) > 12;
+WHERE snapshot_date LIKE '__/__/____' 
+    AND SUBSTRING(snapshot_date, 4, 2) > 12;
 
+-- DD/MM/YYYY day validation check 
 SELECT
     snapshot_date
 FROM bronze.inventory_snapshots
-WHERE snapshot_date LIKE '__/__/____' AND LEFT(snapshot_date, 2) > 12;
---
+WHERE snapshot_date LIKE '__/__/____' 
+    AND LEFT(snapshot_date, 2) > 12;
+
+-- Mon DD, YYYY format analysis 
 SELECT
     snapshot_date
 FROM bronze.inventory_snapshots
-WHERE snapshot_date LIKE '[A-Z][a-z][a-z] __, ____' ;  -->> DONE
---
+WHERE snapshot_date LIKE '[A-Z][a-z][a-z] __, ____' ;  -- DONE
+
+-- YYYY/MM/DD format analysis 
 SELECT
     snapshot_date
 FROM bronze.inventory_snapshots
-WHERE snapshot_date LIKE '____/__/__' ;  -->> DONE
---
+WHERE snapshot_date LIKE '____/__/__' ;  -- DONE
+
+-- MM-DD-YYYY format analysis 
 SELECT
     snapshot_date
 FROM bronze.inventory_snapshots
 WHERE snapshot_date LIKE '__-__-____' ;
 
+-- MM-DD-YYYY month validation check 
 SELECT
     snapshot_date
 FROM bronze.inventory_snapshots
-WHERE snapshot_date LIKE '__-__-____' AND SUBSTRING(snapshot_date, 4, 2) > 12;
+WHERE snapshot_date LIKE '__-__-____' 
+    AND SUBSTRING(snapshot_date, 4, 2) > 12;
 
+-- DD-MM-YYYY day validation check 
 SELECT
     snapshot_date
 FROM bronze.inventory_snapshots
-WHERE snapshot_date LIKE '__-__-____' AND LEFT(snapshot_date, 2) > 12;
---
-SELECT
-    snapshot_date
-FROM bronze.inventory_snapshots
-WHERE snapshot_date LIKE '____-__-__' ; -->> DONE
---
-SELECT
-    snapshot_date
-FROM bronze.inventory_snapshots
-WHERE snapshot_date LIKE '[A-Z][a-z][a-z][a-z] __, ____' ;  -->> DONE
+WHERE snapshot_date LIKE '__-__-____' 
+    AND LEFT(snapshot_date, 2) > 12;
 
--- date_pattern        pattern_count  percentage      
--- ------------------  -------------  ----------------
--- 99/99/9999          152            27.140000000000%  -->> DONE
--- aaa 99, 9999        96             17.140000000000%  -->> DONE
--- 9999/99/99          84             15.000000000000%  -->> DONE
--- 99-99-9999          77             13.750000000000%
--- 9999-99-99          70             12.500000000000%  -->> DONE
--- aaaaaaa 99, 9999    35             6.250000000000%   -->> DONE
--- aaaa 99, 9999       21             3.750000000000%   -->> DONE
--- aaaaaaaa 99, 9999   16             2.860000000000%   -->> DONE
--- aaaaaaaaa 99, 9999  9              1.610000000000%   -->> DONE
+-- YYYY-MM-DD format analysis 
+SELECT
+    snapshot_date
+FROM bronze.inventory_snapshots
+WHERE snapshot_date LIKE '____-__-__' ;  -- DONE
 
--- fineal query 
+-- Month DD, YYYY format analysis 
+SELECT
+    snapshot_date
+FROM bronze.inventory_snapshots
+WHERE snapshot_date LIKE '[A-Z][a-z][a-z][a-z] __, ____' ;  -- DONE
+
+-- Final snapshot_date cleaning validation 
 WITH snapshot_date_analysis AS 
 (
     SELECT 
         CASE 
-            WHEN snapshot_date LIKE '[A-Z][a-z][a-z][a-z] __, ____' THEN TRY_CONVERT(DATE ,snapshot_date)
-            WHEN snapshot_date LIKE '[A-Z][a-z][a-z] __, ____'      THEN TRY_CONVERT(DATE ,snapshot_date)
-            WHEN snapshot_date LIKE '____/__/__'                    THEN TRY_CONVERT(DATE ,snapshot_date)
-            WHEN snapshot_date LIKE '____-__-__'                    THEN TRY_CONVERT(DATE ,snapshot_date)
+            WHEN snapshot_date LIKE '[A-Z][a-z][a-z][a-z] __, ____' THEN TRY_CONVERT(DATE, snapshot_date)
+            WHEN snapshot_date LIKE '[A-Z][a-z][a-z] __, ____'      THEN TRY_CONVERT(DATE, snapshot_date)
+            WHEN snapshot_date LIKE '____/__/__'                    THEN TRY_CONVERT(DATE, snapshot_date)
+            WHEN snapshot_date LIKE '____-__-__'                    THEN TRY_CONVERT(DATE, snapshot_date)
 
             WHEN snapshot_date LIKE '__/__/____' AND SUBSTRING(snapshot_date, 4, 2) > 12 THEN TRY_CONVERT(DATE, snapshot_date, 101)
             WHEN snapshot_date LIKE '__/__/____' AND LEFT(snapshot_date, 2) > 12         THEN TRY_CONVERT(DATE, snapshot_date, 103)
             WHEN snapshot_date LIKE '__-__-____' AND SUBSTRING(snapshot_date, 4, 2) > 12 THEN TRY_CONVERT(DATE, snapshot_date, 110)
             WHEN snapshot_date LIKE '__-__-____' AND LEFT(snapshot_date, 2) > 12         THEN TRY_CONVERT(DATE, snapshot_date, 105)
             ELSE TRY_CONVERT(DATE, snapshot_date)
-        END as snapshot_date
+        END AS snapshot_date
     FROM bronze.inventory_snapshots
 )
 SELECT 
     snapshot_date
 FROM snapshot_date_analysis
 WHERE snapshot_date IS NULL ;
+
+--=============================================================================================
+--================================= product_id column cleaning ================================
+--=============================================================================================
+
 
 --#############################################################################################
 --############################## EMPLOYEE CLEAN DATA ##########################################
@@ -155,7 +163,7 @@ SELECT TOP (1000)
       ,[product_id]
 
       ,[product_name]
-      
+
       ,[sku]
       ,[category]
       ,[stock_on_hand]
